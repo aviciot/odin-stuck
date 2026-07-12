c973871a34d4dd99d9254684c97346b5de336b3b
aviciot <avicoiot@gmail.com>
2026-07-12 09:17:16 +0000

    style: canvas logo 4x larger (720×576)

--- frontend/src/app/admin/applications/page.tsx
+++ frontend/src/app/admin/applications/page.tsx
@@ -1150,7 +1150,7 @@ function CanvasLogo({ state }: { state: LogoState }) {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="180" height="144"
+          width="720" height="576"
           viewBox="0 0 422 336"
           fill="none"
         >
